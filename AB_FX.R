4fbd52050c9d04e10c72cb0d0fcf80e0f635e809
znott <68050004+znott@users.noreply.github.com>
2020-09-02 16:04:38 +1000

    Update AB_FX.R

--- AB_FX.R
+++ AB_FX.R
@@ -1,8 +1,8 @@
 ### written by K. Garner, April 2020
-### edited by Z. Nott, July 2020
+### edited by Z. Nott, Sept 2020
 ### for the project 'On the detectability of effects in executive function and implicit learning tasks'
 ### Garner, KG*, Nydam, A*, Nott, Z., & Dux, PE 
-
+  
 rm(list=ls())
 ### run analysis of sample size x effect size variability on the AB data
 # ----------------------------------------------------------------------------------------------------
@@ -16,23 +16,47 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working direct
 library(tidyverse) # for data wrangling
 library(wesanderson) # palette for some sweet figure colours
 library(cowplot)
-source("efilids_functions.R") # custom functions written for this project
-source("R_rainclouds.R") # functions for plotting
-
 
 # load data and wrangle into tidy form (see https://r4ds.had.co.nz/tidy-data.html), plus relabel to make
 # labels a little simpler
-dat = read.csv("../total_of_313_subs_AB_task_trial_level_data.csv", header=TRUE)
+dat = read.csv("total_of_313_subs_AB_task_trial_level_data.csv", header=TRUE)
 
-
-
-##### ------------------
+##### T2|T1: Trial Type x Task Order
+##### --------------------------------------------------------
 task.order.T2gT1 <- dat %>% group_by(Subj.No, Task.Order, Trial.Type) %>%
-                            summarise(acc=mean(T2T1.Accuracy))
+  summarise(acc=mean(T2T1.Accuracy))
 task.order.T2gT1$Trial.Type <- factor(task.order.T2gT1$Trial.Type)
 task.order.T2gT1$Task.Order <- factor(task.order.T2gT1$Task.Order)
 task.order.T2gT1$Subj.No <- factor(task.order.T2gT1$Subj.No)
 
-task.order.T2gT1 %>% ggplot(aes(x=Trial.Type, y=acc, fill=Task.Order)) +
-                     geom_boxplot() +
-                     facet_wrap(~Task.Order)
+t.o <- task.order.T2gT1 %>% ggplot(aes(x=Task.Order, y=acc, fill=Trial.Type)) +
+  geom_boxplot() +
+  facet_wrap(~Trial.Type)
+t.o
+
+##### T2|T1: Trial Type x Experimenter
+##### --------------------------------------------------------
+
+experimenter.T2gT1 <- dat %>% group_by(Subj.No, Experimenter, Trial.Type) %>%
+  summarise(acc=mean(T2T1.Accuracy))
+experimenter.T2gT1$Subj.No <- factor(experimenter.T2gT1$Subj.No)
+experimenter.T2gT1$Trial.Type <- factor(experimenter.T2gT1$Trial.Type)
+
+ex <- experimenter.T2gT1 %>% ggplot(aes(x=Experimenter, y=acc, fill=Trial.Type,)) +
+  geom_boxplot() +
+  facet_wrap(~Trial.Type)
+ex
+
+#### use cowplot to make a grid
+####-----------------------------------------------------
+p = plot_grid(t.o, ex, labels=c('A', 'B'), label_size = 12)
+
+p # print out the plot so you can see it
+
+# define variables for saving plots
+plot.fname = "AB_FX.png"
+width = 30 # in inches
+height = 10
+
+#save output
+p = p + ggsave(plot.fname, width = width, height = height, units="in", limitsize = FALSE)
